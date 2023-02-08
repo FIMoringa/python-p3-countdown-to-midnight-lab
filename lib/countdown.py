97cd199ec6bdbd863fbc4dc102705567500c0bea
Titus Muthomi <muthomititus+github@gmail.com>
2023-02-08 10:34:41 +0300

    Add midnight in 5 function to pass test

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,12 @@
 # your code goes here!
+def countdown():
+  print("Midnight in 5 seconds")
+  timer = 5
+  while timer > 0:
+    print(timer)
+    timer -= 1
+
+  print("midnight")
+
+
+countdown()
